15c2354ae88e3305239498df52316293d37eab86
Caio Souza <caiobsouza@hotmail.com>
2018-01-03 14:09:38 -0300

    Written bookDetails test, but won't compile.

--- src/com/twu/biblioteca/core/BibliotecaCore.java
+++ src/com/twu/biblioteca/core/BibliotecaCore.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca.core;
 
 public class BibliotecaCore {
+    String[] books;
+
+    public BibliotecaCore(String[] books) {
+        this.books = new String[books.length];
+
+        for (int i = 0; i < books.length; i++) {
+            this.books[i] = books[i];
+        }
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
 }
--- test/com/twu/biblioteca/core/BibliotecaCoreTest.java
+++ test/com/twu/biblioteca/core/BibliotecaCoreTest.java
@@ -1,4 +1,38 @@
 package com.twu.biblioteca.core;
 
-public class BibliotecaCore {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BibliotecaCoreTest {
+
+    BibliotecaCore core;
+
+    @Before
+    public void setUp() {
+        core = new BibliotecaCore(new String[]{"Test-Driven Development", "Head First Java"});
+    }
+
+    @Test
+    public void testListBooks() {
+        String[] books = core.getBooks();
+
+        assertNotNull(books);
+        assertEquals(2, books.length);
+        assertEquals("Test-Driven Development", books[0]);
+        assertEquals("Head First Java", books[1]);
+    }
+
+    @Test
+    public void testBookDetails() {
+        Book[] books = core.getBooks();
+
+        assertNotNull(books[0]);
+        assertEquals("Test-Driven Development", books[0].getTitle());
+        assertEquals("Kent Beck", books[0].getAuthor());
+        assertEquals(2000, books[0].getYearPublished());
+    }
 }
