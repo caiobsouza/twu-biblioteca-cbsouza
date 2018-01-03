c18a9ff38ca16bd5a8b77205d0b36cdd7fed7d23
Caio Souza <caiobsouza@hotmail.com>
2018-01-03 11:42:55 -0300

    Creating ConsoleHelper Test Class and setting up the tests.

---
+++ src/com/twu/biblioteca/util/ConsoleHelper.java
@@ -0,0 +1,6 @@
+package com.twu.biblioteca.util;
+
+public class ConsoleHelper {
+    public static void showMessage(String s) {
+    }
+}
---
+++ test/com/twu/biblioteca/util/ConsoleHelperTest.java
@@ -0,0 +1,30 @@
+package com.twu.biblioteca.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConsoleHelperTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testWelcomeMessage(){
+        ConsoleHelper.showMessage("Welcome to Our Library!");
+        assertEquals("Welcome to Our Library", outContent.toString());
+    }
+}
