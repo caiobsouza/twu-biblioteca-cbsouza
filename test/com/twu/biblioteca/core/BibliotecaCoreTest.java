1f3f90aceb4429d7445a6109cfb7f3fc4ebf9420
Caio Souza <caiobsouza@hotmail.com>
2018-01-03 14:59:39 -0300

    Written listMenuOptions test, but won't compile.

--- test/com/twu/biblioteca/core/BibliotecaCoreTest.java
+++ test/com/twu/biblioteca/core/BibliotecaCoreTest.java
@@ -1,9 +1,9 @@
 package com.twu.biblioteca.core;
 
 import com.twu.biblioteca.entity.Book;
+
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
---
+++ test/com/twu/biblioteca/util/MainMenuTest.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca.util;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class MainMenuTest {
+    private MainMenu menu;
+
+    @Before
+    public void setUp() {
+        menu = new MainMenu(new String[]{
+                "1. List Books"
+        });
+    }
+
+    @Test
+    public void testMenuOptions(){
+        String[] options = menu.getOptions();
+
+        assertNotNull(options);
+        assertEquals(1, options.length);
+        assertEquals("1. List Books", options[0]);
+    }
+}
