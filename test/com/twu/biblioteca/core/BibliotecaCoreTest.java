8fbed74a62e3c04d9cbefe8012fe8e0af23c4e19
Caio Souza <caiobsouza@hotmail.com>
2018-01-04 12:09:39 -0300

    [Red] Implemented test for book checkout success message.

--- test/com/twu/biblioteca/core/BibliotecaCoreTest.java
+++ test/com/twu/biblioteca/core/BibliotecaCoreTest.java
@@ -2,14 +2,19 @@ package com.twu.biblioteca.core;
 
 import com.twu.biblioteca.entity.Book;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 public class BibliotecaCoreTest {
 
     private BibliotecaCore core;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
     public void setUp() {
@@ -18,6 +23,13 @@ public class BibliotecaCoreTest {
                         new Book("Test-Driven Development", "Kent Beck", 2000),
                         new Book("Head First Java", "Clark Kent", 2004)
                 });
+
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
     }
 
     @Test
@@ -41,11 +53,20 @@ public class BibliotecaCoreTest {
     }
 
     @Test
-    public void testBookCheckinOut(){
+    public void testBookCheckout(){
         Book book = core.getBooks()[0];
 
         assertTrue(book.getIsAvailable());
         book.checkOut();
         assertFalse(book.getIsAvailable());
     }
+
+    @Test
+    public void testBookCheckoutSuccessMessage(){
+        Book book = core.getBooks()[0];
+        book.checkOut();
+        assertEquals("Thank you! Enjoy the book", outContent.toString().trim());
+    }
+
+
 }
